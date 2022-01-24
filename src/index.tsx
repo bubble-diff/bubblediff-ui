b070c87a08d0ebde6ad9026fb1c255853972a154
peanutzhen <astzls213@gmail.com>
2022-01-24 16:29:43 +0800

    使用AntD侧边布局

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// create-react-app 将 index.js 作为入口
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// AntD components need this below.
+import "antd/dist/antd.min.css";
+// 全局样式，所有自定义的style都定义于此。
+import "./global.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
